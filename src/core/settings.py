2f9e43b8101f4f369df795e486780bb2b5e801c0
avishekpaudel <avishekpaudel93@gmail.com>
2026-08-31 21:51:28 +0545

    Refactor Docker configuration and update environment variables for PostgreSQL integration

--- src/core/settings.py
+++ src/core/settings.py
@@ -119,11 +119,11 @@ WSGI_APPLICATION = "core.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.getenv("DB_NAME", "pasal"),
-        'USER': os.getenv("DB_USER", "postgres"),
-        'PASSWORD': os.getenv("DB_PASSWORD", "password"),
-        'HOST': os.getenv("DB_HOST", "postgres"),
-        'PORT': '5432',
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD': os.getenv('DB_PASSWORD'),
+        'HOST': os.getenv('DB_HOST'),
+        'PORT': os.getenv('DB_PORT', '5432'),
     }
 }
 
